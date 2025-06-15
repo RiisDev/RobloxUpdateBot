befec13fc35f7e5aa0884d7f72a5d7a53d6f0582
Iris <39096994+RiisDev@users.noreply.github.com>
2025-06-14 20:11:22 -0600

    Rework IOS

--- Services/UpdateService.cs
+++ Services/UpdateService.cs
@@ -91,30 +91,38 @@ namespace RobloxUpdateBot.Services
 
             await UpdateDetected(newStatus, lastVersion);
         }
-        private async Task IosVersionWatcher(string statusKey, string storeUrl)
+
+        private async Task IosVersionWatcherNew(string statusKey, string storeUrl)
         {
             Status? currentStatus = _databaseService.GetStatus(statusKey);
             if (currentStatus == null) return;
 
-            string lastUpdate = currentStatus.Version;
+            string lastVersion = currentStatus.Version;
             HttpRequestMessage request = new(HttpMethod.Get, storeUrl);
             HttpResponseMessage response = await _client.SendAsync(request);
             if (!response.IsSuccessStatusCode) return;
-            
-            IosResult? iosResult = await response.Content.ReadFromJsonAsync<IosResult>();
-            if (iosResult is null || iosResult.Results.Count == 0) return;
-            if (iosResult.Results[0].Version == lastUpdate) return;
+
+            string content = await response.Content.ReadAsStringAsync();
+
+            Match versionMatch = Regex.Match(content, @"Version\s+(\d{1,4}\.\d{1,4}\.\d{1,5})");
+
+            if (!versionMatch.Success) return;
+
+            string currentUpdate = versionMatch.Groups[1].Value;
+
+            if (lastVersion == currentUpdate) return;
 
             Status newStatus = currentStatus with
             {
-                Version = iosResult.Results[0].Version,
+                Version = currentUpdate,
                 Updated = false
             };
 
             _databaseService.UpdateStatus(newStatus);
-            await UpdateDetected(newStatus, lastUpdate);
+            await UpdateDetected(newStatus, lastVersion);
         }
-        
+
+
         private async Task AndroidVersionWatcher(string statusKey, string storeUrl)
         {
             Status? currentStatus = _databaseService.GetStatus(statusKey);
@@ -156,8 +164,8 @@ namespace RobloxUpdateBot.Services
                 [
                     WindowsVersionWatcher(),
                     MacVersionWatcher(),
-                    IosVersionWatcher("IOS", "https://itunes.apple.com/search?term=id431946152&country=us&entity=software"),
-                    IosVersionWatcher("IOS-VNG", "https://itunes.apple.com/search?term=id6474715805&country=vn&entity=software"),
+                    IosVersionWatcherNew("IOS", "https://apps.apple.com/us/app/roblox/id431946152?uo=4"),
+                    IosVersionWatcherNew("IOS-VNG", "https://apps.apple.com/vn/app/roblox-vn/id6474715805?uo=4"),
                     AndroidVersionWatcher("Android", "https://play.google.com/store/apps/details?id=com.roblox.client&hl=en"),
                     AndroidVersionWatcher("Android-VNG", "https://play.google.com/store/apps/details?id=com.roblox.client.vnggames&hl=en")
                 ];
@@ -199,6 +207,85 @@ namespace RobloxUpdateBot.Services
                 ]
             });
         }
+
+        #region OldIos
+        public record IosSubResult(
+            [property: JsonPropertyName("isGameCenterEnabled")] bool? IsGameCenterEnabled,
+            [property: JsonPropertyName("features")] IReadOnlyList<string> Features,
+            [property: JsonPropertyName("advisories")] IReadOnlyList<string> Advisories,
+            [property: JsonPropertyName("supportedDevices")] IReadOnlyList<string> SupportedDevices,
+            [property: JsonPropertyName("kind")] string Kind,
+            [property: JsonPropertyName("artistViewUrl")] string ArtistViewUrl,
+            [property: JsonPropertyName("artworkUrl60")] string ArtworkUrl60,
+            [property: JsonPropertyName("artworkUrl100")] string ArtworkUrl100,
+            [property: JsonPropertyName("screenshotUrls")] IReadOnlyList<string> ScreenshotUrls,
+            [property: JsonPropertyName("ipadScreenshotUrls")] IReadOnlyList<string> IpadScreenshotUrls,
+            [property: JsonPropertyName("appletvScreenshotUrls")] IReadOnlyList<object> AppletvScreenshotUrls,
+            [property: JsonPropertyName("artworkUrl512")] string ArtworkUrl512,
+            [property: JsonPropertyName("artistId")] int? ArtistId,
+            [property: JsonPropertyName("artistName")] string ArtistName,
+            [property: JsonPropertyName("genres")] IReadOnlyList<string> Genres,
+            [property: JsonPropertyName("price")] double? Price,
+            [property: JsonPropertyName("trackId")] long? TrackId,
+            [property: JsonPropertyName("trackName")] string TrackName,
+            [property: JsonPropertyName("bundleId")] string BundleId,
+            [property: JsonPropertyName("isVppDeviceBasedLicensingEnabled")] bool? IsVppDeviceBasedLicensingEnabled,
+            [property: JsonPropertyName("releaseDate")] DateTime? ReleaseDate,
+            [property: JsonPropertyName("primaryGenreName")] string PrimaryGenreName,
+            [property: JsonPropertyName("primaryGenreId")] int? PrimaryGenreId,
+            [property: JsonPropertyName("sellerName")] string SellerName,
+            [property: JsonPropertyName("genreIds")] IReadOnlyList<string> GenreIds,
+            [property: JsonPropertyName("currentVersionReleaseDate")] DateTime? CurrentVersionReleaseDate,
+            [property: JsonPropertyName("releaseNotes")] string ReleaseNotes,
+            [property: JsonPropertyName("version")] string Version,
+            [property: JsonPropertyName("wrapperType")] string WrapperType,
+            [property: JsonPropertyName("currency")] string Currency,
+            [property: JsonPropertyName("description")] string Description,
+            [property: JsonPropertyName("averageUserRating")] double? AverageUserRating,
+            [property: JsonPropertyName("trackCensoredName")] string TrackCensoredName,
+            [property: JsonPropertyName("trackViewUrl")] string TrackViewUrl,
+            [property: JsonPropertyName("contentAdvisoryRating")] string ContentAdvisoryRating,
+            [property: JsonPropertyName("minimumOsVersion")] string MinimumOsVersion,
+            [property: JsonPropertyName("averageUserRatingForCurrentVersion")] double? AverageUserRatingForCurrentVersion,
+            [property: JsonPropertyName("sellerUrl")] string SellerUrl,
+            [property: JsonPropertyName("languageCodesISO2A")] IReadOnlyList<string> LanguageCodesIso2A,
+            [property: JsonPropertyName("fileSizeBytes")] string FileSizeBytes,
+            [property: JsonPropertyName("formattedPrice")] string FormattedPrice,
+            [property: JsonPropertyName("userRatingCountForCurrentVersion")] int? UserRatingCountForCurrentVersion,
+            [property: JsonPropertyName("trackContentRating")] string TrackContentRating,
+            [property: JsonPropertyName("userRatingCount")] int? UserRatingCount
+        );
+
+        public record IosResult(
+            [property: JsonPropertyName("resultCount")] int? ResultCount,
+            [property: JsonPropertyName("results")] IReadOnlyList<IosSubResult> Results
+        );
+
+        private async Task IosVersionWatcher(string statusKey, string storeUrl)
+        {
+            Status? currentStatus = _databaseService.GetStatus(statusKey);
+            if (currentStatus == null) return;
+
+            string lastUpdate = currentStatus.Version;
+            HttpRequestMessage request = new(HttpMethod.Get, storeUrl);
+            HttpResponseMessage response = await _client.SendAsync(request);
+            if (!response.IsSuccessStatusCode) return;
+
+            IosResult? iosResult = await response.Content.ReadFromJsonAsync<IosResult>();
+            if (iosResult is null || iosResult.Results.Count == 0) return;
+            if (iosResult.Results[0].Version == lastUpdate) return;
+
+            Status newStatus = currentStatus with
+            {
+                Version = iosResult.Results[0].Version,
+                Updated = false
+            };
+
+            _databaseService.UpdateStatus(newStatus);
+            await UpdateDetected(newStatus, lastUpdate);
+        }
+
+        #endregion
     }
 
     public record RobloxVersion(
@@ -206,58 +293,4 @@ namespace RobloxUpdateBot.Services
         [property: JsonPropertyName("clientVersionUpload")] string ClientVersionUpload,
         [property: JsonPropertyName("bootstrapperVersion")] string BootstrapperVersion
     );
-
-    public record IosSubResult(
-        [property: JsonPropertyName("isGameCenterEnabled")] bool? IsGameCenterEnabled,
-        [property: JsonPropertyName("features")] IReadOnlyList<string> Features,
-        [property: JsonPropertyName("advisories")] IReadOnlyList<string> Advisories,
-        [property: JsonPropertyName("supportedDevices")] IReadOnlyList<string> SupportedDevices,
-        [property: JsonPropertyName("kind")] string Kind,
-        [property: JsonPropertyName("artistViewUrl")] string ArtistViewUrl,
-        [property: JsonPropertyName("artworkUrl60")] string ArtworkUrl60,
-        [property: JsonPropertyName("artworkUrl100")] string ArtworkUrl100,
-        [property: JsonPropertyName("screenshotUrls")] IReadOnlyList<string> ScreenshotUrls,
-        [property: JsonPropertyName("ipadScreenshotUrls")] IReadOnlyList<string> IpadScreenshotUrls,
-        [property: JsonPropertyName("appletvScreenshotUrls")] IReadOnlyList<object> AppletvScreenshotUrls,
-        [property: JsonPropertyName("artworkUrl512")] string ArtworkUrl512,
-        [property: JsonPropertyName("artistId")] int? ArtistId,
-        [property: JsonPropertyName("artistName")] string ArtistName,
-        [property: JsonPropertyName("genres")] IReadOnlyList<string> Genres,
-        [property: JsonPropertyName("price")] double? Price,
-        [property: JsonPropertyName("trackId")] long? TrackId,
-        [property: JsonPropertyName("trackName")] string TrackName,
-        [property: JsonPropertyName("bundleId")] string BundleId,
-        [property: JsonPropertyName("isVppDeviceBasedLicensingEnabled")] bool? IsVppDeviceBasedLicensingEnabled,
-        [property: JsonPropertyName("releaseDate")] DateTime? ReleaseDate,
-        [property: JsonPropertyName("primaryGenreName")] string PrimaryGenreName,
-        [property: JsonPropertyName("primaryGenreId")] int? PrimaryGenreId,
-        [property: JsonPropertyName("sellerName")] string SellerName,
-        [property: JsonPropertyName("genreIds")] IReadOnlyList<string> GenreIds,
-        [property: JsonPropertyName("currentVersionReleaseDate")] DateTime? CurrentVersionReleaseDate,
-        [property: JsonPropertyName("releaseNotes")] string ReleaseNotes,
-        [property: JsonPropertyName("version")] string Version,
-        [property: JsonPropertyName("wrapperType")] string WrapperType,
-        [property: JsonPropertyName("currency")] string Currency,
-        [property: JsonPropertyName("description")] string Description,
-        [property: JsonPropertyName("averageUserRating")] double? AverageUserRating,
-        [property: JsonPropertyName("trackCensoredName")] string TrackCensoredName,
-        [property: JsonPropertyName("trackViewUrl")] string TrackViewUrl,
-        [property: JsonPropertyName("contentAdvisoryRating")] string ContentAdvisoryRating,
-        [property: JsonPropertyName("minimumOsVersion")] string MinimumOsVersion,
-        [property: JsonPropertyName("averageUserRatingForCurrentVersion")] double? AverageUserRatingForCurrentVersion,
-        [property: JsonPropertyName("sellerUrl")] string SellerUrl,
-        [property: JsonPropertyName("languageCodesISO2A")] IReadOnlyList<string> LanguageCodesIso2A,
-        [property: JsonPropertyName("fileSizeBytes")] string FileSizeBytes,
-        [property: JsonPropertyName("formattedPrice")] string FormattedPrice,
-        [property: JsonPropertyName("userRatingCountForCurrentVersion")] int? UserRatingCountForCurrentVersion,
-        [property: JsonPropertyName("trackContentRating")] string TrackContentRating,
-        [property: JsonPropertyName("userRatingCount")] int? UserRatingCount
-    );
-
-    public record IosResult(
-        [property: JsonPropertyName("resultCount")] int? ResultCount,
-        [property: JsonPropertyName("results")] IReadOnlyList<IosSubResult> Results
-    );
-
-
 }
